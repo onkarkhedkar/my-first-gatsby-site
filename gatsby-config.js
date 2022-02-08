9c337e26c729e9327b5b3c628b19806682d07fc3
Bhargav Ditani <bhargavditani@gmail.com>
2022-02-08 16:41:41 +0530

    MDX plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,17 @@ module.exports = {
     title: `Hello Gatsby`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-sass',
+    // 'gatsby-plugin-image',
+    // 'gatsby-plugin-sharp',
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 // @ts-ignore
 import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.scss';
 
 // eslint-disable-next-line react/prop-types
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div className={container}>
       <title>{pageTitle}</title>
@@ -20,10 +29,15 @@ const Layout = ({ pageTitle, children }) => {
               About
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
-        <h1 className={heading}>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle || data?.site?.siteMetadata?.title}</h1>
         {children}
       </main>
     </div>
